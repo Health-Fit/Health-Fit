495675642f3538f8ffd68b4dbff0c7b8d28e4bee
HeeJu Cho <121915790+he2joojo@users.noreply.github.com>
2024-11-10 14:59:09 +0900

    [refactor] Delete Service Interface
    delete Service interfaces

--- src/main/java/com/heemin/ws/model/service/AuthService.java
+++ src/main/java/com/heemin/ws/model/service/AuthService.java
@@ -1,4 +1,13 @@
 package com.heemin.ws.model.service;
 
-public interface AuthService {
+import com.heemin.ws.model.dao.AuthDao;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private AuthDao authDao;
+
+    public AuthService(AuthDao authDao) {
+        this.authDao = authDao;
+    }
 }
